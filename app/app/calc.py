623feca7b17e8c28099063c5de3595bcbbdb268d
Salahuddin Shaikh <salahoddin88@gmail.com>
2023-05-12 01:10:32 +0530

    Configured docker compose and checks to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Function
+"""
+
+
+def add(x, y):
+    """ Add x and y and return result """
+    return x + y
+
+
+def sub(x, y):
+    """ Subtract x and y and return result """
+    return y - x
